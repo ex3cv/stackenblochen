1b6e913aecdcbaf29308650d16346e94688ea41e
Remigiusz Świc <remigiusz@swic.net.pl>
2021-11-05 23:57:47 +0100

    Add prometheus exporter

--- stackenblochen/app.py
+++ stackenblochen/app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+
 
 app = FastAPI()
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
 
 @app.get("/")
 async def root():
     return {"message": "success"}
+
